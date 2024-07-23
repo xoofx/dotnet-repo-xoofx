8d299d12a02cf14c121dad741bbd26bbabb3c3a1
Alexandre Mutel <alexandre_mutel@live.com>
2024-07-23 07:26:01 +0200

    Update project template, add central package management by default, add Verify defaults

--- ProjectName/src/ProjectName.Tests/Class1Tests.cs
+++ ProjectName/src/ProjectName.Tests/Class1Tests.cs
@@ -1,4 +1,6 @@
-using System;
+// Copyright (c) Alexandre Mutel. All rights reserved.
+// Licensed under the BSD-Clause 2 license.
+// See license.txt file in the project root for full license information.
 
 namespace ProjectName.Tests;
 
